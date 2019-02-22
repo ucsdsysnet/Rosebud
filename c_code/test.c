a2e10699b1d01ab2c136076d2783390246cab99b
Moein Khazraee <mkhazrae@cs.ucsd.edu>
2019-02-22 00:45:36 -0800

    added offset in C.

--- c_code/test.c
+++ c_code/test.c
@@ -1,4 +1,4 @@
-inline void process (unsigned short* len, unsigned char* port, unsigned int* data);
+inline void process (unsigned short* len, unsigned char* port, unsigned int* offset, unsigned int* data);
 #define SLOT_COUNT 16
 int main(void){
   volatile unsigned int * trigger = (volatile unsigned int *) 0x00100;
@@ -8,6 +8,7 @@ int main(void){
   unsigned short len;
   unsigned char port;
   unsigned char slot;
+	unsigned int offset=10;
   int i;
   
   while(1){
@@ -18,12 +19,12 @@ int main(void){
   			slot = *(((unsigned char*)&trigger[i])+3);
   			data = (unsigned int*)(trigger[i+1]);
   			trigger[i] = 0;
-  			process (&len,&port,data);
+  			process (&len, &port, &offset, data);
   			// Order of writing to stat is important, last two should 
   			// be to stat and then stat+1 and it should not happen before that. 
   			// there is 10 byte offset when DMA writes and we did not change it,
   			// so that would be the start address of packet. 
-  			*(stat+1) = (unsigned int)data+10;
+  			*(stat+1) = (unsigned int)data+offset;
   			*stat = (int)len;
   			*((unsigned char*)stat+2) = port;
   			*((unsigned char*)stat+3) = slot;
@@ -35,9 +36,10 @@ int main(void){
   return 1;
 }
 
-inline void process (unsigned short * len, unsigned char * port, unsigned int* data) {
+inline void process (unsigned short* len, unsigned char* port, unsigned int *offset, unsigned int* data) {
 	data[6] = data[6]+0x05050505;
 	data[7] = data[7]+0x05050505;
+	*offset = 6;
 	if (*port==0)
 		*port = 1;
 	else
