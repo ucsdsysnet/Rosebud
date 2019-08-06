8175497454d9a5bfe2643aaa9524e7d15770b0cc
Moein Khazraee <mkhazrae@cs.ucsd.edu>
2019-08-05 17:46:23 -0700

    Added item count and almost flags to FIFOs

--- fpga_src/rtl/loaded_desc_fifo.v
+++ fpga_src/rtl/loaded_desc_fifo.v
@@ -1,11 +1,13 @@
 module loaded_desc_fifo # (
-  parameter CORE_COUNT      = 16,
-  parameter SLOT_COUNT      = 8,
-  parameter START_ADDR      = 8'h20,
-  parameter ADDR_STEP       = 8'h08,
-  parameter SLOT_ADDR_WIDTH = 8,
-  parameter ADDR_WIDTH      = $clog2(CORE_COUNT*SLOT_COUNT),
-  parameter DATA_WIDTH      = $clog2(CORE_COUNT)+SLOT_ADDR_WIDTH
+  parameter CORE_COUNT        = 16,
+  parameter SLOT_COUNT        = 8,
+  parameter START_ADDR        = 8'h20,
+  parameter ADDR_STEP         = 8'h08,
+  parameter SLOT_ADDR_WIDTH   = 8,
+  parameter ADDR_WIDTH        = $clog2(CORE_COUNT*SLOT_COUNT),
+  parameter DATA_WIDTH        = $clog2(CORE_COUNT)+SLOT_ADDR_WIDTH,
+  parameter ALMOST_FULL_DIST  = 2, 
+  parameter ALMOST_EMPTY_DIST = 2
 )(
   input clk,
   input rst,
@@ -16,13 +18,22 @@ module loaded_desc_fifo # (
  
   output               		dout_valid,
   output [DATA_WIDTH-1:0] dout,
-  input                		dout_ready
+  input                		dout_ready,
+
+  output [ADDR_WIDTH:0]   item_count,
+  output                  full,
+  output                  empty,
+  output                  almost_full,
+  output                  almost_empty
 );
+
+localparam ALMOST_FULL_THRESH  = 2**ADDR_WIDTH + ALMOST_FULL_DIST;
+localparam ALMOST_EMPTY_THRESH = ALMOST_EMPTY_DIST;
 
 reg [DATA_WIDTH-1:0] mem [0:(2**ADDR_WIDTH)-1];
 reg [ADDR_WIDTH-1:0] rptr, wptr;
+reg [ADDR_WIDTH:0]   item_count_r;
 reg enque_r, deque_r;
-wire empty, full;
 
 wire enque = din_valid  & din_ready;
 wire deque = dout_valid & dout_ready;
@@ -54,11 +65,24 @@ always @ (posedge clk)
 		deque_r <= deque;
 	end
 
+always @ (posedge clk)
+	if (rst)
+    item_count_r <= {{1'b1},{ADDR_WIDTH{1'b0}}};
+  else 
+    if (enque && !deque)
+      item_count_r <= item_count_r + {{(ADDR_WIDTH){1'b0}},1'b1};
+    else if (!enque && deque)
+      item_count_r <= item_count_r - {{(ADDR_WIDTH){1'b0}},1'b1};
+
 assign empty = (rptr == wptr) & deque_r;
 assign full  = (rptr == wptr) & enque_r;
 
 assign din_ready  = ~full & ~rst;
 assign dout_valid = ~empty;
+
+assign almost_full  = item_count_r >= ALMOST_FULL_THRESH;
+assign almost_empty = item_count_r <= ALMOST_EMPTY_THRESH;
+assign item_count   = item_count_r;
 
 integer i,j;
 reg [SLOT_ADDR_WIDTH-1:0] slot_addr;
--- fpga_src/rtl/simple_fifo.v
+++ fpga_src/rtl/simple_fifo.v
@@ -1,6 +1,9 @@
 module simple_fifo # (
   parameter ADDR_WIDTH = 5,
-  parameter DATA_WIDTH = 32
+  parameter DATA_WIDTH = 32,
+
+  parameter ALMOST_FULL_DIST  = 2, 
+  parameter ALMOST_EMPTY_DIST = 2
 )(
   input clk,
   input rst,
@@ -11,13 +14,22 @@ module simple_fifo # (
  
   output               		dout_valid,
   output [DATA_WIDTH-1:0] dout,
-  input                		dout_ready
+  input                		dout_ready,
+
+  output [ADDR_WIDTH:0]   item_count,
+  output                  full,
+  output                  empty,
+  output                  almost_full,
+  output                  almost_empty
 );
+
+localparam ALMOST_FULL_THRESH  = 2**ADDR_WIDTH + ALMOST_FULL_DIST;
+localparam ALMOST_EMPTY_THRESH = ALMOST_EMPTY_DIST;
 
 reg [DATA_WIDTH-1:0] mem [0:(2**ADDR_WIDTH)-1];
 reg [ADDR_WIDTH-1:0] rptr, wptr;
+reg [ADDR_WIDTH:0]   item_count_r;
 reg enque_r, deque_r;
-wire empty, full;
 
 wire enque = din_valid  & din_ready;
 wire deque = dout_valid & dout_ready;
@@ -48,11 +60,23 @@ always @ (posedge clk)
 		deque_r <= deque;
 	end
 
+always @ (posedge clk)
+	if (rst) 
+    item_count_r <= {(ADDR_WIDTH+1){1'b0}};
+  else 
+    if (enque && !deque)
+      item_count_r <= item_count_r + {{(ADDR_WIDTH){1'b0}},1'b1};
+    else if (!enque && deque)
+      item_count_r <= item_count_r - {{(ADDR_WIDTH){1'b0}},1'b1};
+
 assign empty = (rptr == wptr) & deque_r;
 assign full  = (rptr == wptr) & enque_r;
 
 assign din_ready  = ~full & ~rst;
 assign dout_valid = ~empty;
 
-endmodule
+assign almost_full  = item_count_r >= ALMOST_FULL_THRESH;
+assign almost_empty = item_count_r <= ALMOST_EMPTY_THRESH;
+assign item_count   = item_count_r;
 
+endmodule
