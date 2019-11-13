63304ed35b7cdd50ca68dacd6e57d8b696c0731e
Moein Khazraee <mkhazrae@cs.ucsd.edu>
2019-11-13 11:21:54 -0800

    Added basic forward code.

---
+++ c_code/basic_fw.c
@@ -0,0 +1,32 @@
+#include "core.h"
+
+int main(void){
+
+	volatile unsigned short * sh_test  = (volatile unsigned short *) 0x0700A;
+
+	struct Desc packet;
+	unsigned int start_time, end_time;
+	
+	write_timer_interval(0x00000200);
+	set_masks(0x1F); //enable just errors 
+
+	// Do this at the beginnig, so scheduler can fill the slots while 
+	// initializing other things.
+	init_slots(8, 0x200A, 2048);
+	
+	while (1){
+		if (in_pkt_ready()){
+	 		
+			read_in_pkt(&packet);
+
+			if (packet.port==0)
+				packet.port = 1;
+			else
+				packet.port = 0;
+			
+			safe_pkt_done_msg(&packet);
+  	}
+  }
+  
+  return 1;
+}
