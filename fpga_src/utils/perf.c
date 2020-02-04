994d0cdf4154757d2570f15878721952901f1022
Alex Forencich <alex@alexforencich.com>
2020-02-03 18:25:39 -0800

    Improve perf utility output

--- fpga_src/utils/perf.c
+++ fpga_src/utils/perf.c
@@ -37,6 +37,9 @@ either expressed or implied, of The Regents of the University of California.
 
 #include "mqnic.h"
 
+#define MAX_CORE_COUNT 16
+#define MAX_IF_COUNT 4
+
 static void usage(char *name)
 {
     fprintf(stderr,
@@ -100,23 +103,114 @@ int main(int argc, char *argv[])
     printf("IF stride: 0x%08x\n", dev->if_stride);
     printf("IF CSR offset: 0x%08x\n", dev->if_csr_offset);
 
-    for (int k=0; k<16; k++)
+    int core_count = MAX_CORE_COUNT;
+    int if_count = MAX_IF_COUNT;
+
+    uint32_t temp;
+
+    uint32_t core_rx_bytes[MAX_CORE_COUNT];
+    uint32_t core_tx_bytes[MAX_CORE_COUNT];
+    uint32_t core_rx_frames[MAX_CORE_COUNT];
+    uint32_t core_tx_frames[MAX_CORE_COUNT];
+
+    uint32_t core_rx_bytes_raw[MAX_CORE_COUNT];
+    uint32_t core_tx_bytes_raw[MAX_CORE_COUNT];
+    uint32_t core_rx_frames_raw[MAX_CORE_COUNT];
+    uint32_t core_tx_frames_raw[MAX_CORE_COUNT];
+
+    uint32_t if_rx_bytes[MAX_IF_COUNT];
+    uint32_t if_tx_bytes[MAX_IF_COUNT];
+    uint32_t if_rx_frames[MAX_IF_COUNT];
+    uint32_t if_tx_frames[MAX_IF_COUNT];
+
+    uint32_t if_rx_bytes_raw[MAX_IF_COUNT];
+    uint32_t if_tx_bytes_raw[MAX_IF_COUNT];
+    uint32_t if_rx_frames_raw[MAX_IF_COUNT];
+    uint32_t if_tx_frames_raw[MAX_IF_COUNT];
+
+    for (int k=0; k<core_count; k++)
     {
         mqnic_reg_write32(dev->regs, 0x000410, k);
-        printf("core %d has %08x slots\n", k, mqnic_reg_read32(dev->regs, 0x000410));
-        printf("core %d received %08x bytes\n", k, mqnic_reg_read32(dev->regs, 0x000414));
-        printf("core %d sent %08x bytes\n", k, mqnic_reg_read32(dev->regs, 0x000418));
-        printf("core %d received %08x frames\n", k, mqnic_reg_read32(dev->regs, 0x00041C));
-        printf("core %d sent %08x frames\n", k, mqnic_reg_read32(dev->regs, 0x000420));
+        core_rx_bytes[k] = 0;
+        core_tx_bytes[k] = 0;
+        core_rx_frames[k] = 0;
+        core_tx_frames[k] = 0;
+        core_rx_bytes_raw[k] = mqnic_reg_read32(dev->regs, 0x000414);
+        core_tx_bytes_raw[k] = mqnic_reg_read32(dev->regs, 0x000418);
+        core_rx_frames_raw[k] = mqnic_reg_read32(dev->regs, 0x00041C);
+        core_tx_frames_raw[k] = mqnic_reg_read32(dev->regs, 0x000420);
     }
-    
-    for (int k=0; k<4; k++)
+
+    for (int k=0; k<if_count; k++)
     {
         mqnic_reg_write32(dev->regs, 0x000414, k);
-        printf("interface %d received %08x bytes\n", k, mqnic_reg_read32(dev->regs, 0x000424));
-        printf("interface %d sent %08x bytes\n", k, mqnic_reg_read32(dev->regs, 0x000428));
-        printf("interface %d received %08x frames\n", k, mqnic_reg_read32(dev->regs, 0x00042C));
-        printf("interface %d sent %08x frames\n", k, mqnic_reg_read32(dev->regs, 0x000430));
+        if_rx_bytes[k] = 0;
+        if_tx_bytes[k] = 0;
+        if_rx_frames[k] = 0;
+        if_tx_frames[k] = 0;
+        if_rx_bytes_raw[k] = mqnic_reg_read32(dev->regs, 0x000424);
+        if_tx_bytes_raw[k] = mqnic_reg_read32(dev->regs, 0x000428);
+        if_rx_frames_raw[k] = mqnic_reg_read32(dev->regs, 0x00042C);
+        if_tx_frames_raw[k] = mqnic_reg_read32(dev->regs, 0x000430);
+    }
+
+    while (1)
+    {
+        sleep(1);
+
+        for (int k=0; k<core_count; k++)
+        {
+            mqnic_reg_write32(dev->regs, 0x000410, k);
+
+            temp = mqnic_reg_read32(dev->regs, 0x000414);
+            core_rx_bytes[k] = temp = core_rx_bytes_raw[k];
+            core_rx_bytes_raw[k] = temp;
+
+            temp = mqnic_reg_read32(dev->regs, 0x000418);
+            core_tx_bytes[k] = temp = core_tx_bytes_raw[k];
+            core_tx_bytes_raw[k] = temp;
+
+            temp = mqnic_reg_read32(dev->regs, 0x00041C);
+            core_rx_frames[k] = temp = core_rx_frames_raw[k];
+            core_rx_frames_raw[k] = temp;
+
+            temp = mqnic_reg_read32(dev->regs, 0x000420);
+            core_tx_frames[k] = temp = core_tx_frames_raw[k];
+            core_tx_frames_raw[k] = temp;
+        }
+
+        for (int k=0; k<if_count; k++)
+        {
+            mqnic_reg_write32(dev->regs, 0x000414, k);
+
+            temp = mqnic_reg_read32(dev->regs, 0x000424);
+            if_rx_bytes[k] = temp = if_rx_bytes_raw[k];
+            if_rx_bytes_raw[k] = temp;
+
+            temp = mqnic_reg_read32(dev->regs, 0x000428);
+            if_tx_bytes[k] = temp = if_tx_bytes_raw[k];
+            if_tx_bytes_raw[k] = temp;
+
+            temp = mqnic_reg_read32(dev->regs, 0x00042C);
+            if_rx_frames[k] = temp = if_rx_frames_raw[k];
+            if_rx_frames_raw[k] = temp;
+
+            temp = mqnic_reg_read32(dev->regs, 0x000430);
+            if_tx_frames[k] = temp = if_tx_frames_raw[k];
+            if_tx_frames_raw[k] = temp;
+        }
+
+        printf("           RX bytes    TX bytes   RX frames   TX frames\n");
+
+        for (int k=0; k<core_count; k++)
+        {
+            printf("core %2d    %8d    %8d    %8d    %8d\n", k, core_rx_bytes[k], core_tx_bytes[k], core_rx_frames[k], core_tx_frames[k]);
+        }
+
+        for (int k=0; k<if_count; k++)
+        {
+            printf("if   %2d    %8d    %8d    %8d    %8d\n", k, if_rx_bytes[k], if_tx_bytes[k], if_rx_frames[k], if_tx_frames[k]);
+        }
     }
 
     mqnic_close(dev);
