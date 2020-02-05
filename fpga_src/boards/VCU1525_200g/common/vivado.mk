82e6667611737b6d01c0ebcb838f5edd7cf2955a
Moein Khazraee <mkhazrae@cs.ucsd.edu>
2020-02-04 22:02:41 -0800

    Added 200G design, updated Makefile for C compile, fixed core2.h

---
+++ fpga_src/boards/VCU1525_200g/common/vivado.mk
@@ -0,0 +1,146 @@
+###################################################################
+# 
+# Xilinx Vivado FPGA Makefile
+# 
+# Copyright (c) 2016 Alex Forencich
+# 
+###################################################################
+# 
+# Parameters:
+# FPGA_TOP - Top module name
+# FPGA_FAMILY - FPGA family (e.g. VirtexUltrascale)
+# FPGA_DEVICE - FPGA device (e.g. xcvu095-ffva2104-2-e)
+# SYN_FILES - space-separated list of source files
+# INC_FILES - space-separated list of include files
+# XDC_FILES - space-separated list of timing constraint files
+# XCI_FILES - space-separated list of IP XCI files
+# 
+# Example:
+# 
+# FPGA_TOP = fpga
+# FPGA_FAMILY = VirtexUltrascale
+# FPGA_DEVICE = xcvu095-ffva2104-2-e
+# SYN_FILES = rtl/fpga.v
+# XDC_FILES = fpga.xdc
+# XCI_FILES = ip/pcspma.xci
+# include ../common/vivado.mk
+# 
+###################################################################
+
+# phony targets
+.PHONY: clean fpga
+
+# prevent make from deleting intermediate files and reports
+.PRECIOUS: %.xpr %.bit %.mcs %.prm
+.SECONDARY:
+
+CONFIG ?= config.mk
+-include ../$(CONFIG)
+
+SYN_FILES_REL = $(patsubst %, ../%, $(SYN_FILES))
+INC_FILES_REL = $(patsubst %, ../%, $(INC_FILES))
+XCI_FILES_REL = $(patsubst %, ../%, $(XCI_FILES))
+
+ifdef XDC_FILES
+  XDC_FILES_REL = $(patsubst %, ../%, $(XDC_FILES))
+else
+  XDC_FILES_REL = $(FPGA_TOP).xdc
+endif
+
+###################################################################
+# Main Targets
+#
+# all: build everything
+# clean: remove output files and project files
+###################################################################
+
+all: fpga
+
+fpga: $(FPGA_TOP).bit
+
+tmpclean:
+	-rm -rf *.log *.jou *.cache *.hw *.ip_user_files *.runs *.xpr *.html *.xml *.sim *.srcs *.str .Xil defines.v rev *.rpt hd_visual
+	-rm -rf create_project.tcl run_synth.tcl run_impl.tcl generate_bit.tcl
+
+clean: tmpclean
+	-rm -rf *.bit program.tcl generate_mcs.tcl *.mcs *.prm flash.tcl
+
+distclean: clean
+	-rm -rf rev
+
+###################################################################
+# Target implementations
+###################################################################
+
+# Vivado project file
+%.xpr: Makefile $(XCI_FILES_REL)
+	rm -rf defines.v
+	touch defines.v
+	for x in $(DEFS); do echo '`define' $$x >> defines.v; done
+	echo "create_project -force -part $(FPGA_PART) $*" > create_project.tcl
+	echo "add_files -fileset sources_1 defines.v" >> create_project.tcl
+	for x in $(SYN_FILES_REL); do echo "add_files -fileset sources_1 $$x" >> create_project.tcl; done
+	for x in $(XDC_FILES_REL); do echo "add_files -fileset constrs_1 $$x" >> create_project.tcl; done
+	for x in $(XCI_FILES_REL); do echo "import_ip $$x" >> create_project.tcl; done
+	echo "exit" >> create_project.tcl
+	vivado -nojournal -nolog -mode batch -source create_project.tcl
+
+# synthesis run
+%.runs/synth_1/%.dcp: %.xpr $(SYN_FILES_REL) $(INC_FILES_REL) $(XDC_FILES_REL)
+	echo "open_project $*.xpr" > run_synth.tcl
+	echo "set_property PR_FLOW 1 [current_project]" >> run_synth.tcl
+	echo "if {[llength [get_partition_defs  "pr_riscv"]]==0} then {create_partition_def -name pr_riscv -module riscv_block_PR}" >> run_synth.tcl
+	echo "if {[llength [get_reconfig_modules "riscv_block_PR"]]==0} then {create_reconfig_module -name riscv_block_PR -partition_def [get_partition_defs pr_riscv ]  -define_from riscv_block_PR}" >> run_synth.tcl
+	echo "update_compile_order -fileset riscv_block_PR" >> run_synth.tcl
+	echo "update_compile_order -fileset sources_1" >> run_synth.tcl
+	echo "reset_run synth_1" >> run_synth.tcl
+	echo "launch_runs synth_1" >> run_synth.tcl
+	echo "wait_on_run synth_1" >> run_synth.tcl
+	echo "exit" >> run_synth.tcl
+	vivado -nojournal -nolog -mode batch -source run_synth.tcl
+
+# implementation run
+%.runs/impl_1/%_routed.dcp: %.runs/synth_1/%.dcp
+	echo "open_project $*.xpr" > run_impl.tcl
+	echo "set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs impl_1]" >> run_impl.tcl
+	echo "set_property PR_FLOW 1 [current_project]" >> run_impl.tcl
+	echo "if {[llength [get_pr_configurations  "config_1"]]==0} then {create_pr_configuration -name config_1 -partitions [list core_inst/riscv_cores[0].pr_wrapper:riscv_block_PR core_inst/riscv_cores[1].pr_wrapper:riscv_block_PR core_inst/riscv_cores[2].pr_wrapper:riscv_block_PR core_inst/riscv_cores[3].pr_wrapper:riscv_block_PR core_inst/riscv_cores[4].pr_wrapper:riscv_block_PR core_inst/riscv_cores[5].pr_wrapper:riscv_block_PR core_inst/riscv_cores[6].pr_wrapper:riscv_block_PR core_inst/riscv_cores[7].pr_wrapper:riscv_block_PR core_inst/riscv_cores[8].pr_wrapper:riscv_block_PR core_inst/riscv_cores[9].pr_wrapper:riscv_block_PR core_inst/riscv_cores[10].pr_wrapper:riscv_block_PR core_inst/riscv_cores[11].pr_wrapper:riscv_block_PR core_inst/riscv_cores[12].pr_wrapper:riscv_block_PR core_inst/riscv_cores[13].pr_wrapper:riscv_block_PR core_inst/riscv_cores[14].pr_wrapper:riscv_block_PR core_inst/riscv_cores[15].pr_wrapper:riscv_block_PR ]}" >> run_impl.tcl
+	echo "set_property PR_CONFIGURATION config_1 [get_runs impl_1]" >> run_impl.tcl
+	echo "set_property strategy Performance_ExtraTimingOpt [get_runs impl_1]" >> run_impl.tcl
+	echo "reset_run impl_1" >> run_impl.tcl
+	echo "launch_runs impl_1" >> run_impl.tcl
+	echo "wait_on_run impl_1" >> run_impl.tcl
+	echo "exit" >> run_impl.tcl
+	vivado -nojournal -nolog -mode batch -source run_impl.tcl
+
+%.runs/impl_2/%_routed.dcp: %.runs/synth_1/%.dcp
+	echo "open_project $*.xpr" > run_impl.tcl
+	echo "if {[llength [get_pr_configurations  "config_2"]]==0} then {create_pr_configuration -name config_2 -partitions { }  -greyboxes [list core_inst/riscv_cores[0].pr_wrapper core_inst/riscv_cores[1].pr_wrapper core_inst/riscv_cores[2].pr_wrapper core_inst/riscv_cores[3].pr_wrapper core_inst/riscv_cores[4].pr_wrapper core_inst/riscv_cores[5].pr_wrapper core_inst/riscv_cores[6].pr_wrapper core_inst/riscv_cores[7].pr_wrapper core_inst/riscv_cores[8].pr_wrapper core_inst/riscv_cores[9].pr_wrapper core_inst/riscv_cores[10].pr_wrapper core_inst/riscv_cores[11].pr_wrapper core_inst/riscv_cores[12].pr_wrapper core_inst/riscv_cores[13].pr_wrapper core_inst/riscv_cores[14].pr_wrapper core_inst/riscv_cores[15].pr_wrapper ]}" >> run_impl.tcl
+	echo "if {[llength [get_runs  "impl_2"]]==0} then {create_run impl_2 -parent_run impl_1 -flow {Vivado Implementation 2019} -pr_config config_2}" >> run_impl.tcl
+	echo "set_property strategy Performance_ExtraTimingOpt [get_runs impl_2]" >> run_impl.tcl
+	echo "reset_run impl_2" >> run_impl.tcl
+	echo "launch_runs impl_2" >> run_impl.tcl
+	echo "wait_on_run impl_2" >> run_impl.tcl
+	echo "exit" >> run_impl.tcl
+	vivado -nojournal -nolog -mode batch -source run_impl.tcl
+
+# bit file
+%.bit: %.runs/impl_1/%_routed.dcp %.runs/impl_2/%_routed.dcp
+	echo "open_project $*.xpr" > generate_bit.tcl
+	echo "open_run impl_1" >> generate_bit.tcl
+	echo "write_debug_probes -force debug_probes.ltx" >> generate_bit.tcl
+	echo "report_utilization -force -hierarchical -hierarchical_percentage -file fpga_utilization_hierarchy_placed_full.rpt" >> generate_bit.tcl
+	echo "report_utilization -force -pblocks [get_pblocks -regexp {pblock_([2-9]|1[0-6]|1)}] -file fpga_utilization_pblocks.rpt" >> generate_bit.tcl
+	echo "write_bitstream -force $*.runs/impl_1/$*.bit" >> generate_bit.tcl
+	echo "open_run impl_2" >> generate_bit.tcl
+	echo "write_debug_probes -force debug_probes.ltx" >> generate_bit.tcl
+	echo "report_utilization -force -hierarchical -hierarchical_percentage -file fpga_utilization_hierarchy_placed_grey.rpt" >> generate_bit.tcl
+	echo "write_bitstream -force $*.runs/impl_2/$*.bit" >> generate_bit.tcl
+	echo "exit" >> generate_bit.tcl
+	vivado -nojournal -nolog -mode batch -source generate_bit.tcl
+	mkdir -p rev
+	EXT=bit; COUNT=100; \
+	while [ -e rev/$*_rev$$COUNT.$$EXT ]; \
+	do COUNT=$$((COUNT+1)); done; \
+	cp $@ rev/$*_rev$$COUNT.$$EXT; \
+	echo "Output: rev/$*_rev$$COUNT.$$EXT";
---
+++ fpga_src/boards/VCU1525_200g/rtl/riscv_block_PR.v
@@ -0,0 +1,370 @@
+module riscv_block_PR (  
+  input  wire         sys_clk,
+  input               sys_rst,
+  input  wire         core_rst,
+
+  input  wire [3:0]   core_id,
+  input  wire         core_interrupt,
+  output reg          core_interrupt_ack,
+  
+  // DMA interface
+  input  wire         dma_cmd_wr_en,
+  input  wire [21:0]  dma_cmd_wr_addr,
+  input  wire         dma_cmd_hdr_wr_en,
+  input  wire [21:0]  dma_cmd_hdr_wr_addr,
+  input  wire [127:0] dma_cmd_wr_data,
+  input  wire [15:0]  dma_cmd_wr_strb,
+  input  wire         dma_cmd_wr_last,
+  output wire         dma_cmd_wr_ready,
+  
+  input  wire         dma_cmd_rd_en,
+  input  wire [21:0]  dma_cmd_rd_addr,
+  input  wire         dma_cmd_rd_last,
+  output wire         dma_cmd_rd_ready,
+  
+  output wire         dma_rd_resp_valid,
+  output wire [127:0] dma_rd_resp_data,
+  input  wire         dma_rd_resp_ready,
+    
+  // Descriptor to/from core 
+  input  wire [63:0]  in_desc,
+  input  wire         in_desc_valid,
+  output wire         in_desc_taken,
+
+  output wire [63:0]  out_desc,
+  output wire [63:0]  out_desc_dram_addr,
+  output wire         out_desc_valid,
+  input  wire         out_desc_ready,
+
+  // Slot information from core
+  output wire [3:0]   slot_wr_ptr, 
+  output wire [21:0]  slot_wr_addr,
+  output wire         slot_wr_valid,
+  output wire         slot_for_hdr,
+  input  wire         slot_wr_ready,
+ 
+  // Received DRAM infor to core
+  input  wire [4:0]   recv_dram_tag,
+  input  wire         recv_dram_tag_valid,
+
+  // Broadcast messages
+  input  wire [46:0]  bc_msg_in,
+  input  wire         bc_msg_in_valid,
+  output wire [46:0]  bc_msg_out,
+  output wire         bc_msg_out_valid,
+  input  wire         bc_msg_out_ready
+);
+
+// Parameters that should match the wrapper and are used in ports
+parameter DATA_WIDTH     = 128;
+parameter STRB_WIDTH     = (DATA_WIDTH/8);
+parameter IMEM_SIZE      = 65536;
+parameter SLOW_DMEM_SIZE = 1048576;
+parameter FAST_DMEM_SIZE = 32768;
+parameter SLOW_M_B_LINES = 4096;
+parameter FAST_M_B_LINES = 1024;
+parameter BC_REGION_SIZE = 8192;
+parameter BC_START_ADDR  = SLOW_DMEM_SIZE+FAST_DMEM_SIZE-BC_REGION_SIZE;
+parameter MSG_WIDTH      = 32+4+$clog2(BC_REGION_SIZE)-2;
+parameter ADDR_WIDTH     = $clog2(SLOW_DMEM_SIZE)+2;
+parameter CORE_ID_WIDTH  = 4;
+parameter SLOT_COUNT     = 8;
+parameter SLOT_WIDTH     = $clog2(SLOT_COUNT+1);
+
+// Internal paramaters
+parameter LINE_ADDR_BITS       = $clog2(STRB_WIDTH);
+parameter SLOW_DMEM_ADDR_WIDTH = $clog2(SLOW_DMEM_SIZE);
+parameter FAST_DMEM_ADDR_WIDTH = $clog2(FAST_DMEM_SIZE);
+parameter IMEM_ADDR_WIDTH      = $clog2(IMEM_SIZE);
+//parameter BC_MSG_ADDR_WIDTH    = $clog2(BC_REGION_SIZE);
+parameter REG_TYPE             = 2;
+parameter REG_LENGTH           = 2;
+
+parameter ACC_ADDR_WIDTH       = $clog2(SLOW_M_B_LINES);
+parameter SLOW_DMEM_SEL_BITS   = SLOW_DMEM_ADDR_WIDTH-$clog2(STRB_WIDTH)
+                                 -1-$clog2(SLOW_M_B_LINES);
+parameter ACC_MEM_BLOCKS       = 2**SLOW_DMEM_SEL_BITS;
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////// Register input and outputs ////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+(* KEEP = "TRUE" *) reg sys_rst_r;
+(* KEEP = "TRUE" *) reg core_rst_r;
+
+reg  [3:0] core_id_r;
+reg        core_interrupt_r;
+wire       core_interrupt_ack_n;
+
+always @ (posedge sys_clk) begin
+  sys_rst_r          <= sys_rst;
+  core_rst_r         <= core_rst;
+  core_id_r          <= core_id;
+  core_interrupt_r   <= core_interrupt;
+  core_interrupt_ack <= core_interrupt_ack_n;
+end
+
+wire                  dma_cmd_wr_en_r;
+wire [ADDR_WIDTH-1:0] dma_cmd_wr_addr_r;
+wire                  dma_cmd_hdr_wr_en_r;
+wire [ADDR_WIDTH-1:0] dma_cmd_hdr_wr_addr_r;
+wire [DATA_WIDTH-1:0] dma_cmd_wr_data_r;
+wire [STRB_WIDTH-1:0] dma_cmd_wr_strb_r;
+wire                  dma_cmd_wr_last_r;
+wire                  dma_cmd_wr_ready_r;
+
+simple_pipe_reg # (
+  .DATA_WIDTH(DATA_WIDTH+(2*ADDR_WIDTH)+STRB_WIDTH+2),
+  .REG_TYPE(REG_TYPE), 
+  .REG_LENGTH(REG_LENGTH)
+) dma_wr_reg (
+  .clk(sys_clk),
+  .rst(sys_rst_r),
+  
+  .s_data({dma_cmd_wr_data, dma_cmd_wr_addr, dma_cmd_hdr_wr_addr,
+           dma_cmd_hdr_wr_en, dma_cmd_wr_strb, dma_cmd_wr_last}),
+  .s_valid(dma_cmd_wr_en),
+  .s_ready(dma_cmd_wr_ready),
+
+  .m_data({dma_cmd_wr_data_r, dma_cmd_wr_addr_r, dma_cmd_hdr_wr_addr_r,
+           dma_cmd_hdr_wr_en_r, dma_cmd_wr_strb_r, dma_cmd_wr_last_r}),
+  .m_valid(dma_cmd_wr_en_r),
+  .m_ready(dma_cmd_wr_ready_r)
+);
+
+wire                  dma_cmd_rd_en_r;
+wire [ADDR_WIDTH-1:0] dma_cmd_rd_addr_r;
+wire                  dma_cmd_rd_last_r;
+wire                  dma_cmd_rd_ready_r;
+
+simple_pipe_reg # (
+  .DATA_WIDTH(ADDR_WIDTH+1),
+  .REG_TYPE(REG_TYPE), 
+  .REG_LENGTH(REG_LENGTH)
+) dma_rd_reg (
+  .clk(sys_clk),
+  .rst(sys_rst_r),
+  
+  .s_data({dma_cmd_rd_addr,dma_cmd_rd_last}),
+  .s_valid(dma_cmd_rd_en),
+  .s_ready(dma_cmd_rd_ready),
+
+  .m_data({dma_cmd_rd_addr_r,dma_cmd_rd_last_r}),
+  .m_valid(dma_cmd_rd_en_r),
+  .m_ready(dma_cmd_rd_ready_r)
+);
+
+wire                  dma_rd_resp_valid_n;
+wire [DATA_WIDTH-1:0] dma_rd_resp_data_n;
+wire                  dma_rd_resp_ready_n;
+
+simple_pipe_reg # (
+  .DATA_WIDTH(DATA_WIDTH),
+  .REG_TYPE(REG_TYPE), 
+  .REG_LENGTH(REG_LENGTH)
+) dma_rd_resp_reg (
+  .clk(sys_clk),
+  .rst(sys_rst_r),
+  
+  .s_data(dma_rd_resp_data_n),
+  .s_valid(dma_rd_resp_valid_n),
+  .s_ready(dma_rd_resp_ready_n),
+
+  .m_data(dma_rd_resp_data),
+  .m_valid(dma_rd_resp_valid),
+  .m_ready(dma_rd_resp_ready)
+);
+    
+wire [63:0] in_desc_r;
+wire        in_desc_valid_r;
+wire        in_desc_taken_r;
+
+simple_pipe_reg # (
+  .DATA_WIDTH(64),
+  .REG_TYPE(REG_TYPE), 
+  .REG_LENGTH(REG_LENGTH)
+) in_desc_reg (
+  .clk(sys_clk),
+  .rst(sys_rst_r),
+  
+  .s_data(in_desc),
+  .s_valid(in_desc_valid),
+  .s_ready(in_desc_taken),
+
+  .m_data(in_desc_r),
+  .m_valid(in_desc_valid_r),
+  .m_ready(in_desc_taken_r)
+);
+
+wire [63:0] out_desc_n;
+wire [63:0] out_desc_dram_addr_n;
+wire        out_desc_valid_n;
+wire        out_desc_ready_n;
+
+simple_pipe_reg # (
+  .DATA_WIDTH(128),
+  .REG_TYPE(REG_TYPE), 
+  .REG_LENGTH(REG_LENGTH)
+) out_desc_reg (
+  .clk(sys_clk),
+  .rst(sys_rst_r),
+  
+  .s_data({out_desc_dram_addr_n,out_desc_n}),
+  .s_valid(out_desc_valid_n),
+  .s_ready(out_desc_ready_n),
+
+  .m_data({out_desc_dram_addr,out_desc}),
+  .m_valid(out_desc_valid),
+  .m_ready(out_desc_ready)
+);
+
+wire [SLOT_WIDTH-1:0] slot_wr_ptr_n;
+wire [ADDR_WIDTH-1:0] slot_wr_addr_n;
+wire                  slot_wr_valid_n;
+wire                  slot_for_hdr_n;
+wire                  slot_wr_ready_n;
+
+simple_pipe_reg # (
+  .DATA_WIDTH(ADDR_WIDTH+SLOT_WIDTH+1),
+  .REG_TYPE(REG_TYPE), 
+  .REG_LENGTH(REG_LENGTH)
+) slot_info_reg (
+  .clk(sys_clk),
+  .rst(sys_rst_r),
+  
+  .s_data({slot_wr_ptr_n, slot_wr_addr_n, slot_for_hdr_n}),
+  .s_valid(slot_wr_valid_n),
+  .s_ready(slot_wr_ready_n),
+
+  .m_data({slot_wr_ptr, slot_wr_addr, slot_for_hdr}),
+  .m_valid(slot_wr_valid),
+  .m_ready(slot_wr_ready)
+);
+
+wire [4:0] recv_dram_tag_r;
+wire       recv_dram_tag_valid_r;
+
+simple_pipe_reg # (
+  .DATA_WIDTH(5), 
+  .REG_TYPE(REG_TYPE), 
+  .REG_LENGTH(REG_LENGTH)
+) dram_tag_reg (
+  .clk(sys_clk),
+  .rst(sys_rst_r),
+  
+  .s_data(recv_dram_tag),
+  .s_valid(recv_dram_tag_valid),
+  .s_ready(),
+
+  .m_data(recv_dram_tag_r),
+  .m_valid(recv_dram_tag_valid_r),
+  .m_ready(1'b1)
+);
+
+wire [MSG_WIDTH-1:0]  bc_msg_in_r;
+wire                  bc_msg_in_valid_r;
+
+simple_pipe_reg # (
+  .DATA_WIDTH(MSG_WIDTH), 
+  .REG_TYPE(REG_TYPE), 
+  .REG_LENGTH(REG_LENGTH)
+) bc_msg_in_reg (
+  .clk(sys_clk),
+  .rst(sys_rst_r),
+  
+  .s_data(bc_msg_in),
+  .s_valid(bc_msg_in_valid),
+  .s_ready(),
+
+  .m_data(bc_msg_in_r),
+  .m_valid(bc_msg_in_valid_r),
+  .m_ready(1'b1)
+);
+
+wire [MSG_WIDTH-1:0]  bc_msg_out_n;
+wire                  bc_msg_out_valid_n;
+wire                  bc_msg_out_ready_n;
+
+simple_pipe_reg # (
+  .DATA_WIDTH(MSG_WIDTH), 
+  .REG_TYPE(REG_TYPE), 
+  .REG_LENGTH(REG_LENGTH)
+) bc_msg_out_reg (
+  .clk(sys_clk),
+  .rst(sys_rst_r),
+  
+  .s_data(bc_msg_out_n),
+  .s_valid(bc_msg_out_valid_n),
+  .s_ready(bc_msg_out_ready_n),
+
+  .m_data(bc_msg_out),
+  .m_valid(bc_msg_out_valid),
+  .m_ready(bc_msg_out_ready)
+);
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////// RISCV BLOCK  ///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+riscv_block # (
+    .DATA_WIDTH(DATA_WIDTH),
+    .STRB_WIDTH(STRB_WIDTH),
+    .IMEM_SIZE(IMEM_SIZE),
+    .SLOW_DMEM_SIZE(SLOW_DMEM_SIZE),
+    .FAST_DMEM_SIZE(FAST_DMEM_SIZE),
+    .SLOW_M_B_LINES(SLOW_M_B_LINES),
+    .FAST_M_B_LINES(FAST_M_B_LINES),
+    .BC_REGION_SIZE(BC_REGION_SIZE),
+    .BC_START_ADDR(BC_START_ADDR),
+    .MSG_WIDTH(MSG_WIDTH),
+    .CORE_ID_WIDTH(CORE_ID_WIDTH),
+    .SLOT_COUNT(SLOT_COUNT),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .SLOT_WIDTH(SLOT_WIDTH)
+) riscv_block_inst (
+    .sys_clk(sys_clk),
+    .sys_rst(sys_rst_r),
+    .core_rst(core_rst_r),
+
+    .core_id(core_id_r),
+    .core_interrupt(core_interrupt_r),
+    .core_interrupt_ack(core_interrupt_ack_n),
+
+    .dma_cmd_wr_en(dma_cmd_wr_en_r),
+    .dma_cmd_wr_addr(dma_cmd_wr_addr_r),
+    .dma_cmd_hdr_wr_en(dma_cmd_hdr_wr_en_r),
+    .dma_cmd_hdr_wr_addr(dma_cmd_hdr_wr_addr_r),
+    .dma_cmd_wr_data(dma_cmd_wr_data_r),
+    .dma_cmd_wr_strb(dma_cmd_wr_strb_r),
+    .dma_cmd_wr_last(dma_cmd_wr_last_r),
+    .dma_cmd_wr_ready(dma_cmd_wr_ready_r),
+    .dma_cmd_rd_en(dma_cmd_rd_en_r),
+    .dma_cmd_rd_addr(dma_cmd_rd_addr_r),
+    .dma_cmd_rd_last(dma_cmd_rd_last_r),
+    .dma_cmd_rd_ready(dma_cmd_rd_ready_r),
+    .dma_rd_resp_valid(dma_rd_resp_valid_n),
+    .dma_rd_resp_data(dma_rd_resp_data_n),
+    .dma_rd_resp_ready(dma_rd_resp_ready_n),
+
+    .in_desc(in_desc_r),
+    .in_desc_valid(in_desc_valid_r),
+    .in_desc_taken(in_desc_taken_r),
+    .out_desc(out_desc_n),
+    .out_desc_dram_addr(out_desc_dram_addr_n),
+    .out_desc_valid(out_desc_valid_n),
+    .out_desc_ready(out_desc_ready_n),
+
+    .slot_wr_ptr(slot_wr_ptr_n),
+    .slot_wr_addr(slot_wr_addr_n),
+    .slot_wr_valid(slot_wr_valid_n),
+    .slot_for_hdr(slot_for_hdr_n),
+    .slot_wr_ready(slot_wr_ready_n),
+    .recv_dram_tag(recv_dram_tag_r),
+    .recv_dram_tag_valid(recv_dram_tag_valid_r),
+
+    .bc_msg_out(bc_msg_out_n),
+    .bc_msg_out_valid(bc_msg_out_valid_n),
+    .bc_msg_out_ready(bc_msg_out_ready_n),
+    .bc_msg_in(bc_msg_in_r),
+    .bc_msg_in_valid(bc_msg_in_valid_r)
+);
+
+endmodule
