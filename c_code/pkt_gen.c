32df52f80b806751a88a1bdcdde27178d18ed9ea
Moein <mkhazrae@cs.ucsd.edu>
2020-02-05 20:24:47 -0800

    Made the pkt_gen to run in 19 cycles for both ports

--- c_code/pkt_gen.c
+++ c_code/pkt_gen.c
@@ -1,14 +1,14 @@
 #include "core2.h"
+
+#define two_ports 1
+
 struct Desc packet;
 unsigned int * pkt_data[16];
 
 int main(void){
 
   int i;
-  unsigned int shift;
   unsigned int pkt_num;
-
-	shift = (core_id()>>2) & 0x1;
 
 	write_timer_interval(0x00000200);
 	set_masks(0x1F); //enable just errors 
@@ -18,25 +18,31 @@ int main(void){
 	init_hdr_slots(8, 0x104000, 128);
 	init_slots(8, 0x00000A, 16384);
 
-  for (i=0;i<16;i++){
+	for (i=0;i<16;i++){
 		pkt_data[i] = (unsigned int *)(0+i*16384);
 		pkt_data[i][0] = core_id();
 	}
 
 	pkt_num    = 0;
-	packet.len = 1500;
+	packet.len = 64;
 	packet.tag = 0;
-	// packet.port = 0;
+
+	if (two_ports==1)
+		if ((core_id() & 0x4)==0)
+			packet.port = 0;
+		else
+			packet.port = 1;
+	else
+		packet.port = 0;
 
 	while (1){
-    for (i=0;i<16;i++) {
-	    pkt_data[i][1] = pkt_num;
-	    packet.port = (i+shift) & 0x1;
-		  packet.data = (unsigned char *) pkt_data[i];
-		  safe_pkt_send(&packet);
-		  pkt_num++;
-	  } 
-  }
-  
+		for (i=0;i<16;i++) {
+			pkt_data[i][1] = pkt_num;
+			packet.data = (unsigned char *) pkt_data[i];
+			safe_pkt_send(&packet);
+			pkt_num++;
+		} 
+	}
+
   return 1;
 }
