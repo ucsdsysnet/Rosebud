a2c1d7dade1473a2541a5f32ab8a4ad2e2b5bcf6
Moein <mkhazrae@cs.ucsd.edu>
2020-03-03 19:51:44 -0800

    Improved tx performance of latency test.

--- c_code/latency.c
+++ c_code/latency.c
@@ -8,7 +8,7 @@ unsigned int * pkt_data[8];
 unsigned int * wr_ptr;
 unsigned int count;
 
-#define congestion 1
+#define congestion 0
 
 void __attribute__((interrupt)) int_handler(void) {
   safe_pkt_send(&summary_pkt);
@@ -32,14 +32,14 @@ int main(void){
     pkt_data[i] = (unsigned int *)(0x01000000+(i*16384));
   }
 
-  send_pkt.len = 64;
+  send_pkt.len = 9000;
   send_pkt.tag = 0;
   // Half of the cores send, so one port is enough
   send_pkt.port = 0;
 
-  summary_pkt.len=128;
-  summary_pkt.port=2;
+  summary_pkt.len=1500;
   summary_pkt.tag=0;
+  summary_pkt.port=2;
 
   wr_ptr = (unsigned int *) 0x1080000;
   summary_pkt.data=((unsigned char*) wr_ptr);
@@ -55,14 +55,13 @@ int main(void){
 
   if ((core_id()&0x1)==0){
     while (1){
-      if (congestion==0)
-          for (k=0;k<1000;k++);
-      send_pkt.data = (unsigned char *) pkt_data[i];
-      pkt_data[i][0] = read_timer_low();
-      safe_pkt_send(&send_pkt);
-      i++;
-      if (i==8)
-        i=0;
+      for (i=0;i<8;i++) {
+        if (congestion==0)
+          for (k=0;k<100000;k++);
+        send_pkt.data = (unsigned char *) pkt_data[i];
+        pkt_data[i][0] = read_timer_low();
+        safe_pkt_send(&send_pkt);
+      }
     }
   } else {
     while (1) {
@@ -73,8 +72,6 @@ int main(void){
           // Write send and recv time into memory and update the pointer
           *wr_ptr = recv_time - (*((unsigned int *)recv_pkt.data)); 
           wr_ptr+=1;
-          if (wr_ptr == (unsigned int *) 0x10F0000)
-            wr_ptr = (unsigned int *) 0x1080000;
           
           if (congestion==0){
             count+=4;
@@ -83,7 +80,10 @@ int main(void){
               count = 0; 
               wr_ptr = (unsigned int *) 0x1080000;
             }
-          }
+          } else {
+            if (wr_ptr == (unsigned int *) 0x10F0000)
+              wr_ptr = (unsigned int *) 0x1080000;
+	  }
         }
         // Drop the packet
         recv_pkt.len=0;
