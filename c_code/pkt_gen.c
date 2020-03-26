8b4f48c1b52453d2e189e2cd0e9ee6d8980a1d05
Moein <mkhazrae@cs.ucsd.edu>
2020-03-25 19:20:14 -0700

    Added benchmark results (not in figures yet). Cleaned up file locations and some c codes.

--- c_code/pkt_gen.c
+++ c_code/pkt_gen.c
@@ -7,33 +7,35 @@ int main(void){
 
   int i;
   unsigned int pkt_num;
+  volatile int k;
 
-	// Do this at the beginnig, so scheduler can fill the slots while 
-	// initializing other things.
-	init_hdr_slots(8, 0x804000, 128);
-	init_slots(8, 0x00000A, 16384);
+  // Do this at the beginnig, so scheduler can fill the slots while 
+  // initializing other things.
+  init_hdr_slots(8, 0x804000, 128);
+  init_slots(8, 0x00000A, 16384);
 
-	for (i=0;i<16;i++){
-		pkt_data[i] = (unsigned int *)(0x01000000+i*16384);
-		pkt_data[i][0] = core_id();
-	}
+  for (i=0;i<16;i++){
+    pkt_data[i] = (unsigned int *)(0x01000000+i*16384);
+    pkt_data[i][0] = core_id();
+  }
 
-	pkt_num    = 0;
-	packet.len = 64;
-	packet.tag = 0;
-	if ((core_id()&0x4)!=0)
-		packet.port = 1;
-	else
-		packet.port = 0;
+  pkt_num    = 0;
+  packet.len = 4096;
+  packet.tag = 0;
+  if ((core_id()&0x4)!=0)
+    packet.port = 1;
+  else
+    packet.port = 0;
 
-	while (1){
-		for (i=0;i<16;i++) {
-			pkt_data[i][1] = pkt_num;
-			packet.data = (unsigned char *) pkt_data[i];
-			pkt_send(&packet);
-			pkt_num++;
-		} 
-	}
+  while (1){
+    for (i=0;i<16;i++) {
+      // for (k=0; k<10000; k++);
+      pkt_data[i][1] = pkt_num;
+      packet.data = (unsigned char *) pkt_data[i];
+      pkt_send(&packet);
+      pkt_num++;
+    } 
+  }
 
   return 1;
 }
