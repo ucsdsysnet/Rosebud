5fe1fb8d572fdc2282ad847f9be0614680dc16ab
Moein <mkhazrae@cs.ucsd.edu>
2020-04-06 16:14:21 -0700

    Added 0 initialization for pakcet generator scripts.

--- c_code/table_gen.py
+++ c_code/table_gen.py
@@ -1,7 +1,15 @@
 data = (256*1024/2)*[0,1]
 # print (data)
-
 with open('table.bin', 'wb') as f:
   f.write(str(bytearray(data)))
+f.close()
 
+data = (32*1024)*[0]
+with open('empty_dmem.bin', 'wb') as f:
+  f.write(str(bytearray(data)))
+f.close()
+
+data = (1024*1024)*[0]
+with open('empty_pmem.bin', 'wb') as f:
+  f.write(str(bytearray(data)))
 f.close()
