8a5124fda1031a28956388f2ab08c4680fe513b4
Moein <mkhazrae@cs.ucsd.edu>
2020-04-05 18:39:35 -0700

    Updated basic_fw_hash to ignore packets from host and bounce them back, added hash_test.c to generate UDP packets.

--- c_code/basic_fw_hash.c
+++ c_code/basic_fw_hash.c
@@ -16,9 +16,9 @@
 #define ACC_HASH_READ  (*((volatile unsigned int *)(IO_EXT_BASE + 0x0110)))
 
 struct slot_context {
-	int index;
-	struct Desc desc;
-	unsigned char *header;
+  int index;
+  struct Desc desc;
+  unsigned char *header;
 };
 
 struct slot_context context[MAX_CTX_COUNT];
@@ -30,90 +30,95 @@ unsigned int header_slot_size;
 
 inline void slot_rx_packet(struct slot_context *ctx)
 {
-	long int hash;
+  long int hash;
 
-	// parse header and compute flow hash
-	
-	// reset hash module
-	ACC_HASH_CTRL = 0;
+  // parse header and compute flow hash
 
-	// check eth type
-	if (*((unsigned short *)(ctx->header+12)) == 0x0008)
-	{
-		// IPv4 packet
-		// IPv4 addresses
+  // reset hash module
+  ACC_HASH_CTRL = 0;
+
+  // check eth type
+  if (*((unsigned short *)(ctx->header+12)) == 0x0008)
+  {
+    // IPv4 packet
+    // IPv4 addresses
     ACC_HASH_DWORD = *((unsigned int *)(ctx->header+26));
-		ACC_HASH_DWORD = *((unsigned int *)(ctx->header+30));
+    ACC_HASH_DWORD = *((unsigned int *)(ctx->header+30));
 
-		// check IHL and protocol
-		if (ctx->header[14] == 0x45 && (ctx->header[23] == 0x06 || ctx->header[23] == 0x11))
-		{
-			// TCP or UDP ports
-			ACC_HASH_DWORD = *((unsigned int *)(ctx->header+34));
-		}
-	
+    // check IHL and protocol
+    if (ctx->header[14] == 0x45 && (ctx->header[23] == 0x06 || ctx->header[23] == 0x11))
+    {
+      // TCP or UDP ports
+      ACC_HASH_DWORD = *((unsigned int *)(ctx->header+34));
+    }
+
   // read hash
-	hash = ACC_HASH_READ;
-	}
+  hash = ACC_HASH_READ;
+  }
 
-	// swap port
-	if (ctx->desc.port==0)
-		ctx->desc.port = 1;
-	else
-		ctx->desc.port = 0;
-	
-	pkt_send(&ctx->desc);
+  // swap port
+  if (ctx->desc.port==0)
+    ctx->desc.port = 1;
+  else
+    ctx->desc.port = 0;
+
+  pkt_send(&ctx->desc);
 }
 
 int main(void)
 {
-	// set slot configuration parameters
-	slot_count = PMEM_SEG_COUNT;
-	slot_size = PMEM_SEG_SIZE;
-	header_slot_base = DMEM_BASE + (DMEM_SIZE >> 1);
-	header_slot_size = 128;
+  // set slot configuration parameters
+  slot_count = PMEM_SEG_COUNT;
+  slot_size = PMEM_SEG_SIZE;
+  header_slot_base = DMEM_BASE + (DMEM_SIZE >> 1);
+  header_slot_size = 128;
 
-	if (slot_count > MAX_SLOT_COUNT)
-		slot_count = MAX_SLOT_COUNT;
+  if (slot_count > MAX_SLOT_COUNT)
+    slot_count = MAX_SLOT_COUNT;
 
-	if (slot_count > MAX_CTX_COUNT)
-		slot_count = MAX_CTX_COUNT;
+  if (slot_count > MAX_CTX_COUNT)
+    slot_count = MAX_CTX_COUNT;
 
-	// Do this at the beginning, so scheduler can fill the slots while
-	// initializing other things.
-	init_hdr_slots(slot_count, header_slot_base, header_slot_size);
-	init_slots(slot_count, PKT_OFFSET, slot_size);
+  // Do this at the beginning, so scheduler can fill the slots while
+  // initializing other things.
+  init_hdr_slots(slot_count, header_slot_base, header_slot_size);
+  init_slots(slot_count, PKT_OFFSET, slot_size);
 
-	// init slot context structures
-	for (int i = 0; i < slot_count; i++)
-	{
-		context[i].index = i;
-		context[i].desc.tag = i+1;
-		context[i].desc.data = (unsigned char *)(PMEM_BASE + PKT_OFFSET + i*slot_size);
-		context[i].header = (unsigned char *)(header_slot_base + PKT_OFFSET + i*header_slot_size);
-	}
+  // init slot context structures
+  for (int i = 0; i < slot_count; i++)
+  {
+    context[i].index = i;
+    context[i].desc.tag = i+1;
+    context[i].desc.data = (unsigned char *)(PMEM_BASE + PKT_OFFSET + i*slot_size);
+    context[i].header = (unsigned char *)(header_slot_base + PKT_OFFSET + i*header_slot_size);
+  }
 
-	while (1)
-	{
-		// check for new packets
-		if (in_pkt_ready())
-		{
-			struct Desc desc;
-			int index;
+  while (1)
+  {
+    // check for new packets
+    if (in_pkt_ready())
+    {
+      struct Desc desc;
+      int index;
 
-			// read descriptor
-			read_in_pkt(&desc);
+      // read descriptor
+      read_in_pkt(&desc);
 
-			// compute index
-			index = desc.tag-1;
+      // send back the packets from host
+      if (desc.port==2){
+        pkt_send(&desc);
+      } else {
+        // compute index
+        index = desc.tag-1;
 
-			// copy descriptor into context
-			context[index].desc = desc;
+        // copy descriptor into context
+        context[index].desc = desc;
 
-			// handle packet
-			slot_rx_packet(&context[index]);
-		}
-	}
+        // handle packet
+        slot_rx_packet(&context[index]);
+      }
+    }
+  }
 
-	return 1;
+  return 1;
 }
---
+++ c_code/hash_test.c
@@ -0,0 +1,55 @@
+#include "core.h"
+
+struct Desc packet;
+unsigned char * pkt_data[16];
+// unsigned int pkt_len[16] = {65,128,256,512,1024,2048,4096,9000,9000,2048,4096,1024,1500,256,128,1024};
+unsigned int pkt_len[16] = {[0 ... 15] = 1500};
+
+char udp_hdr [34] = "\xAB\xAB\xAB\xAB\xAB\xAB\xCD\xCD\xCD\xCD\xCD\xCD\x08\x00"
+                    "\x45\x00\x05\xCE\x00\x00\x00\x00\x00\x11\x00\x00"
+                    "\xC0\xA8\x01\x01\xC0\xA8\x01\x02";
+
+void basic_memcpy(char *dest, char *src, int n) 
+{ 
+  // unsigned int *csrc  = (unsigned int *)src; 
+  // unsigned int *cdest = (unsigned int *)dest; 
+             
+  for (int i=0; i<n; i++) 
+    dest[i] = src[i]; 
+} 
+
+int main(void){
+
+
+  int i;
+  unsigned int pkt_num;
+  volatile int k;
+
+  // Do this at the beginnig, so scheduler can fill the slots while 
+  // initializing other things.
+  init_hdr_slots(8, 0x804000, 128);
+  init_slots(8, 0x00000A, 16384);
+
+  for (i=0;i<16;i++){
+    pkt_data[i] = (unsigned char *)(0x01000000+i*16384);
+    basic_memcpy(pkt_data[i], udp_hdr, 34);
+  }
+
+  pkt_num    = 0;
+  packet.len = 1500;
+  packet.tag = 0;
+  if ((core_id()&0x4)!=0)
+    packet.port = 1;
+  else
+    packet.port = 0;
+
+  while (1){
+    for (i=0;i<16;i++) {
+      packet.data = pkt_data[i];
+      // packet.len = pkt_len[i];
+      pkt_send(&packet);
+    } 
+  }
+
+  return 1;
+}
