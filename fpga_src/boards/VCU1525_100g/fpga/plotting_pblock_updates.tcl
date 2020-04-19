2d65bc2325a6aa596199cf9808cd422474a1630b
Moein <mkhazrae@cs.ucsd.edu>
2020-04-19 16:21:20 -0700

    Added plotting scripts for pblock name changes and rectangle removals.

---
+++ fpga_src/boards/VCU1525_100g/fpga/plotting_pblock_updates.tcl
@@ -0,0 +1,23 @@
+set_property name Gusheh_1  [get_pblocks pblock_1]
+set_property name Gusheh_2  [get_pblocks pblock_2]
+set_property name Gusheh_3  [get_pblocks pblock_3]
+set_property name Gusheh_4  [get_pblocks pblock_4]
+set_property name Gusheh_5  [get_pblocks pblock_5]
+set_property name Gusheh_6  [get_pblocks pblock_6]
+set_property name Gusheh_7  [get_pblocks pblock_7]
+set_property name Gusheh_8  [get_pblocks pblock_8]
+set_property name Gusheh_9  [get_pblocks pblock_9]
+set_property name Gusheh_10 [get_pblocks pblock_10]
+set_property name Gusheh_11 [get_pblocks pblock_11]
+set_property name Gusheh_12 [get_pblocks pblock_12]
+set_property name Gusheh_13 [get_pblocks pblock_13]
+set_property name Gusheh_14 [get_pblocks pblock_14]
+set_property name Gusheh_15 [get_pblocks pblock_15]
+set_property name Gusheh_16 [get_pblocks pblock_16]
+set_property name Scheduler_block [get_pblocks pblock_35]
+
+resize_pblock pblock_33 -remove_rect {93 1 103 309}
+resize_pblock pblock_33 -remove_rect {86 1 88 185}
+resize_pblock pblock_33 -remove_rect {53 1 81 309}
+resize_pblock pblock_34 -remove_rect {0 312 669 620}
+resize_pblock pblock_36 -remove_rect {415 374 669 558}
