9bd9054ea916340438e381e6d99cce97de604f1e
Moein Khazraee <mkhazrae@cs.ucsd.edu>
2020-09-16 14:10:51 -0700

    Put back one axis_register for rx_fifo and met timing for both scheduler with accelerators. Added the script for new Gousheh, can be cleaned up later.

--- fpga_src/boards/VCU1525_100g/rtl/fpga_core.v
+++ fpga_src/boards/VCU1525_100g/rtl/fpga_core.v
@@ -567,7 +567,7 @@ generate
             .DEST_ENABLE(0),
             .USER_ENABLE(0),
             .REG_TYPE(2),
-            .LENGTH(1)
+            .LENGTH(2)
         ) mac_rx_pipeline (
             .clk(sys_clk),
             .rst(sys_rst),
