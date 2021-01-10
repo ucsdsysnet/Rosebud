9218a57ef1726559af6e4f5c2bb53bc34a770f3b
Alex Forencich <alex@alexforencich.com>
2021-01-09 23:33:08 -0800

    Update table_gen.py for python3, convert to runnable script

--- c_code/table_gen.py
+++ c_code/table_gen.py
@@ -1,20 +1,22 @@
-data = (256*1024/8)*[1,0,0,0,0,0,0,0]
+#!/usr/bin/env python3
+
+data = (256*1024//8)*[1,0,0,0,0,0,0,0]
 # print (data)
 with open('table.bin', 'wb') as f:
-  f.write(str(bytearray(data)))
+    f.write(bytes(data))
 f.close()
 
-data = (256*1024/4)*[1,0,0,0]
+data = (256*1024//4)*[1,0,0,0]
 with open('table_qtr1.bin', 'wb') as f:
-  f.write(str(bytearray(data)))
+    f.write(bytes(data))
 f.close()
 
 data = (32*1024)*[0]
 with open('empty_dmem.bin', 'wb') as f:
-  f.write(str(bytearray(data)))
+    f.write(bytes(data))
 f.close()
 
 data = (1024*1024)*[0]
 with open('empty_pmem.bin', 'wb') as f:
-  f.write(str(bytearray(data)))
+    f.write(bytes(data))
 f.close()
