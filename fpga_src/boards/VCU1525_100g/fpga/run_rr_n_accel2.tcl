8c4b451cbe26fd7cb22999b5eb7fe53f8d97cdc4
Moein Khazraee <mkhazrae@cs.ucsd.edu>
2021-01-15 23:28:49 -0800

    Added the script for running accel implementation before sched.

--- fpga_src/boards/VCU1525_100g/fpga/run_rr_n_accel2.tcl
+++ fpga_src/boards/VCU1525_100g/fpga/run_rr_n_accel2.tcl
@@ -5,6 +5,36 @@ set_property needs_refresh false [get_runs synth_1]
 set_property needs_refresh false [get_runs impl_1]
 # set_property needs_refresh false [get_runs impl_2]
 
+if {[llength [get_reconfig_modules riscv_block_PR_w_accel]]==0} then {
+  create_reconfig_module -name riscv_block_PR_w_accel -partition_def [get_partition_defs pr_riscv ] -top riscv_block_PR_w_accel}
+
+add_files -norecurse {
+  ../lib/eth/lib/axis/rtl/arbiter.v 
+  ../lib/eth/lib/axis/rtl/priority_encoder.v 
+  ../lib/smartFPGA/rtl/core_mems.v
+  ../lib/smartFPGA/rtl/axis_fifo.v
+  ../lib/smartFPGA/rtl/VexRiscv.v
+  ../lib/eth/lib/axis/rtl/axis_register.v
+  ../lib/eth/lib/axis/rtl/axis_pipeline_register.v
+  ../lib/smartFPGA/rtl/simple_sync_sig.v
+  ../lib/smartFPGA/rtl/riscvcore.v
+  ../lib/smartFPGA/rtl/simple_fifo.v
+  ../lib/smartFPGA/rtl/mem_sys.v
+  ../lib/smartFPGA/rtl/riscv_block.v
+  ../lib/smartFPGA/rtl/accel_rd_dma_sp.v
+  ../accel/full_ids/rtl/sme/tcp_sme.v
+  ../accel/full_ids/rtl/sme/udp_sme.v
+  ../accel/full_ids/rtl/sme/http_sme.v
+  ../accel/full_ids/rtl/fixed_sme/fixed_loc_sme_8.v
+  ../accel/full_ids/rtl/ip_match/ip_match.v
+  ../accel/full_ids/rtl/accel_wrap_full_ids.v
+  ../rtl/riscv_block_PR_w_accel.v
+} -of_objects [get_reconfig_modules riscv_block_PR_w_accel]
+  
+  # ../accel/hash/rtl/hash_acc.v
+  # ../accel/merged/rtl/re_sql.v
+  # ../accel/merged/rtl/accel_wrap_merged.v
+
 if {[llength [get_pr_configurations rr_n_accel]]!=0} then {
   delete_pr_configurations rr_n_accel}
 create_pr_configuration -name rr_n_accel -partitions [list \
@@ -30,8 +60,16 @@ if {[llength [get_runs "impl_accel"]]!=0} then {delete_run impl_accel}
 create_run impl_accel -parent_run impl_1 -flow {Vivado Implementation 2020} -pr_config rr_n_accel
 set_property strategy Performance_ExtraTimingOpt [get_runs impl_accel]
 
-# add_files -fileset utils_1 -norecurse ../lib/axis/syn/sync_reset.tcl
-# add_files -fileset utils_1 -norecurse ../lib/smartFPGA/syn/simple_sync_sig.tcl
+update_compile_order -fileset riscv_block_PR_w_accel
+update_compile_order -fileset scheduler_PR
+update_compile_order -fileset sources_1
+
+reset_run riscv_block_PR_w_accel_synth_1
+launch_runs riscv_block_PR_w_accel_synth_1
+wait_on_run riscv_block_PR_w_accel_synth_1
+
+add_files -fileset utils_1 -norecurse ../lib/axis/syn/sync_reset.tcl
+add_files -fileset utils_1 -norecurse ../lib/smartFPGA/syn/simple_sync_sig.tcl
 set_property STEPS.OPT_DESIGN.TCL.PRE [ get_files ../lib/axis/syn/sync_reset.tcl -of [get_fileset utils_1] ] [get_runs impl_accel]
 set_property STEPS.OPT_DESIGN.TCL.PRE [ get_files ../lib/smartFPGA/syn/simple_sync_sig.tcl -of [get_fileset utils_1] ] [get_runs impl_accel]
 set_property STEPS.ROUTE_DESIGN.TCL.PRE [ get_files ../lib/axis/syn/sync_reset.tcl -of [get_fileset utils_1] ] [get_runs impl_accel]
