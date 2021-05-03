9dd809a01e8090257e07ab1e08f024686c82e531
Moein Khazraee <mkhazrae@cs.ucsd.edu>
2021-05-02 21:43:16 -0700

    Merged the fast_pattern extractor to sme_rulecompiler

--- fpga_src/accel/sme/python/sme_rulecompiler.py
+++ fpga_src/accel/sme/python/sme_rulecompiler.py
@@ -33,6 +33,8 @@ either expressed or implied, of The Regents of the University of California.
 import argparse
 import collections
 import os
+import re
+from idstools.rule import parse
 
 
 class AhoCorasickState(object):
@@ -830,6 +832,90 @@ def parse_content_string(s):
     return bytes(b)
 
 
+def truncate_content(content, limit):
+  count = 0
+  byte_mode = 0
+  for i in range(len(content)):
+    if (content[i]=='|'):
+      if (byte_mode==0):
+        byte_mode = 1
+      else:
+        count += 1
+        byte_mode = 0
+    else:
+      if (byte_mode==0):
+        count += 1
+      else:
+        if (content[i]==' '):
+          count += 1
+
+    if (count==limit):
+      if (byte_mode==0):
+        return content[:i+1]
+      else:
+        return (content[:i]+'|')
+
+  return content
+
+
+def fast_pattern_extractor (line):
+  if (len(line.strip()) == 0):
+    return ""
+  else:
+    fast_pattern = ""
+    pattern_len = 0
+    unknown_flag = False
+    fast_flag = False
+
+    rule = parse(line)
+    options = rule['options']
+
+    for opt in options:
+      if opt['name'] == 'content':
+        value = re.findall('"([^"]*)"', opt['value'])[0]
+        flags = opt['value'][len(value)+3:]
+        cont_len = len(parse_content_string(value))
+
+        if "fast_pattern" in flags:
+          fast_pattern = value[:]
+          fast_flag = True
+          if opt['value'].startswith("!"):
+            unknown_flag = True
+          break
+
+        elif opt['value'].startswith("!"):
+
+          # Not acceptable for fast pattern
+          if (len(flags)!=0):
+            if ("relative" in flags) or ("case" in flags) or ("offset" in flags) or \
+              ("depth" in flags) or ("within" in flags) or ("distance" in flags):
+              continue
+
+          # Acceptable but not long enough
+          elif (cont_len <= pattern_len):
+              continue
+
+          # Acceptable, not sure what to do for hardware check
+          # might get overriden by a longer pattern
+          else:
+            unknown_flag = True
+            pattern_len = cont_len
+            continue
+
+        else:
+          # No \" in curent content rules, might not be always true
+          # re strips of the ! and ""
+          if (cont_len>pattern_len):
+            unknown_flag = False
+            fast_pattern = value[:]
+            pattern_len = cont_len
+
+    if unknown_flag:
+      print ("WARNING: used", fast_pattern, "for", line,'\n')
+
+    return fast_pattern
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--split_width', type=int, default=2, help="Bit split width")
@@ -885,12 +971,16 @@ def main():
     for fn in args.rules_file:
         with open(fn, 'r') as f:
             for w in f.read().splitlines():
-                b = parse_content_string(w)
+                pattern = fast_pattern_extractor(w)
+                b = parse_content_string(pattern)
                 if not b:
+                    if w:
+                        print ("ERROR: No patterns found for", w,'\n')
                     continue
                 if max_length:
                     b = b[:max_length]
-                match_list.append((b, w))
+                    pattern=truncate_content(pattern,max_length)
+                match_list.append((b, pattern))
 
     match_set = set()
     match_list_filtered = []
