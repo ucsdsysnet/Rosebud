6b8d11828d79dabe3b2562627d2cc17f61f17489
Alex Forencich <alex@alexforencich.com>
2021-05-07 01:59:02 -0700

    Rework latency test script

--- fpga_src/utils/run_latency.sh
+++ fpga_src/utils/run_latency.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+dev=mqnic0
+eth=$(ls /sys/class/misc/$dev/device/net | head -1)
+pci_id=$(basename $(readlink /sys/class/misc/$dev/device))
+
+mkdir -p latency_res
+
 for i in 64 65 128 256 512 1024 1500 2048 4096 9000; do
-	make -C ../../c_code/ NAME=latency_$i
-  sudo ./pcie_hot_reset.sh 02:00.0
+	make -C ../../c_code/ NAME=latency TARGET=latency_$i SRC=latency.c DEFINES="-DSIZE=$i"
+  sudo ./pcie_hot_reset.sh $pci_id
   sleep 1s
-  sudo ./rvfw -d /dev/mqnic1 -i /home/moein/BumpinTheWire/c_code/latency_$i\_ins.bin -e 0xffff -r 0xaaaa -m /home/moein/BumpinTheWire/c_code/latency.map
-	sudo timeout -k 0 5s ./perf -d /dev/mqnic1 -o latency_$i.csv -g 12
-  sudo tcpdump -c 100 -i eth1 -w latency_res/latency_$i.pcap
-  sudo ./pcie_hot_reset.sh 02:00.0
+  sudo ip link set dev $eth up
+  sudo ./rvfw -d /dev/$dev -i ../../c_code/latency_$i\_ins.bin -e 0xffff -r 0xaaaa -m ../../c_code/latency.map
+  sudo timeout -k 0 5s ./perf -d /dev/$dev -o latency_$i.csv -g 12
+
+  sudo tcpdump -c 100 -i $eth -w latency_res/latency_$i.pcap
+  sudo ./pcie_hot_reset.sh $pci_id
 done
