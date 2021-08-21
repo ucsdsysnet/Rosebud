2ba6d6405966fb8fd0ac84d1378ec4537b1d41f4
Moein Khazraee <mkhazrae@cs.ucsd.edu>
2021-08-21 12:33:04 -0700

    Fixed a bug in C generation, added some counters.

--- fpga_src/accel/pigasus_sme/HLS_test/rules/parser.py
+++ fpga_src/accel/pigasus_sme/HLS_test/rules/parser.py
@@ -36,6 +36,18 @@ if __name__ == "__main__":
   # print("  dst_is_http=false;")
   # print("}\n")
 
+  src_mixed_http_rules = []
+  src_http_rules = []
+  src_single_rules = []
+  src_range_rules = []
+  src_list_rules = []
+
+  dst_mixed_http_rules = []
+  dst_http_rules = []
+  dst_single_rules = []
+  dst_range_rules = []
+  dst_list_rules = []
+
   for line in open("extracted.txt","r"):
     rule = line.split()
     if int(rule[0]) in id_map:
@@ -53,35 +65,46 @@ if __name__ == "__main__":
 
       if (("$HTTP_PORTS" in src_port) and (src_port!="$HTTP_PORTS")):
         src_port = src_port.replace("$HTTP_PORTS", str(HTTP_PORTS)[1:-1])
+        src_mixed_http_rules.append(rule_ID)
       if (src_port.startswith("[")):
         src_port = src_port[1:-1]
 
       if (("$HTTP_PORTS" in dst_port) and (dst_port!="$HTTP_PORTS")):
         dst_port = dst_port.replace("$HTTP_PORTS", str(HTTP_PORTS)[1:-1])
+        dst_mixed_http_rules.append(rule_ID)
       if (dst_port.startswith("[")):
         dst_port = dst_port[1:-1]
 
       if (src_port=="$HTTP_PORTS"):
         src_port_cond=list_cond_gen("src_port", HTTP_PORTS)
+        src_http_rules.append(rule_ID)
       elif (src_port=="$FILE_DATA_PORTS"):
         src_port_cond=list_cond_gen("src_port", HTTP_PORTS+[110,143])
+        src_mixed_http_rules.append(rule_ID)
       elif (src_port=="$FTP_PORTS"):
         src_port_cond=list_cond_gen("src_port", [21,2100,3535])
+        src_list_rules.append(rule_ID)
       elif (src_port=="$ORACLE_PORTS"):
         src_port_cond="(src_port>=1024)"
+        src_range_rules.append(rule_ID)
       elif (":" in src_port):
+        src_range_rules.append(rule_ID)
         if (src_port[-1]==":"):
           ports = [int(x) for x in src_port[:-1].split(",")]
           if (len(ports)==1):
             src_port_cond = "(src_port>="+str(ports[0])+")"
           else:
             src_port_cond = list_cond_gen("src_port", ports[:-1])[:-1]+"|| (src_port>="+str(ports[-1])+") )"
+            if (not rule_ID in src_mixed_http_rules):
+              src_list_rules.append(rule_ID)
         else:
           ports = src_port.split(",")
           if (len(ports)==1):
             [low, high] = ports[0].split(":")
             src_port_cond = "( (src_port>="+low+") && (src_port<="+high+") )"
           else: #limited implementation
+            if (not rule_ID in src_mixed_http_rules):
+              src_list_rules.append(rule_ID)
             if (":" in ports[0]):
               [low, high] = ports[0].split(":")
               src_port_cond = "( ((src_port>="+low+") && (src_port<="+high+")) || (src_port=="+ports[1]+") )"
@@ -93,28 +116,42 @@ if __name__ == "__main__":
         src_port_cond = "true"
       else:
         src_port_cond = list_cond_gen("src_port", [int(x) for x in src_port.split(",")])
+        if (len(src_port.split(","))==1):
+          src_single_rules.append(rule_ID)
+        else:
+          if (not rule_ID in src_mixed_http_rules):
+            src_list_rules.append(rule_ID)
 
       if (dst_port=="$HTTP_PORTS"):
         dst_port_cond=list_cond_gen("dst_port", HTTP_PORTS)
+        dst_http_rules.append(rule_ID)
       elif (dst_port=="$FILE_DATA_PORTS"):
         dst_port_cond=list_cond_gen("dst_port", HTTP_PORTS+[110,143])
+        dst_mixed_http_rules.append(rule_ID)
       elif (dst_port=="$FTP_PORTS"):
         dst_port_cond=list_cond_gen("dst_port", [21,2100,3535])
+        dst_list_rules.append(rule_ID)
       elif (dst_port=="$ORACLE_PORTS"):
         dst_port_cond="(dst_port>=1024)"
+        dst_range_rules.append(rule_ID)
       elif (":" in dst_port):
+        dst_range_rules.append(rule_ID)
         if (dst_port[-1]==":"):
           ports = [int(x) for x in dst_port[:-1].split(",")]
           if (len(ports)==1):
             dst_port_cond = "(dst_port>="+str(ports[0])+")"
           else:
             dst_port_cond = list_cond_gen("dst_port", ports[:-1])[:-1]+"|| (dst_port>="+str(ports[-1])+") )"
+            if (not rule_ID in dst_mixed_http_rules):
+              dst_list_rules.append(rule_ID)
         else:
           ports = dst_port.split(",")
           if (len(ports)==1):
             [low, high] = ports[0].split(":")
             dst_port_cond = "( (dst_port>="+low+") && (dst_port<="+high+") )"
           else: #limited implementation
+            if (not rule_ID in dst_mixed_http_rules):
+              dst_list_rules.append(rule_ID)
             if (":" in ports[0]):
               [low, high] = ports[0].split(":")
               dst_port_cond = "( ((dst_port>="+low+") && (dst_port<="+high+")) || (dst_port=="+ports[1]+") )"
@@ -126,11 +163,32 @@ if __name__ == "__main__":
         dst_port_cond = "true"
       else:
         dst_port_cond = list_cond_gen("dst_port", [int(x) for x in dst_port.split(",")])
+        if (len(dst_port.split(","))==1):
+          dst_single_rules.append(rule_ID)
+        else:
+          if (not rule_ID in dst_mixed_http_rules):
+            dst_list_rules.append(rule_ID)
 
       rule_cond = "( (rule_ID_in=="+str(rule_ID)+") && "+tcp_cond+" && "+src_port_cond+" && "+dst_port_cond+" )"
       rule_conds.append(rule_cond)
 
       rules[rule_ID] = [src_port, dst_port, is_tcp]
+
+  print("len src http, mixed_http, single, range, list: ", len(src_http_rules),  len(src_mixed_http_rules),
+                                    len(src_single_rules), len(src_range_rules), len(src_list_rules))
+  print("min src http, mixed_http, single, range, list: ", min(src_http_rules),  min(src_mixed_http_rules),
+                                    min(src_single_rules), min(src_range_rules), min(src_list_rules))
+  print("max src http, mixed_http, single, range, list: ", max(src_http_rules),  max(src_mixed_http_rules),
+                                    max(src_single_rules), max(src_range_rules), max(src_list_rules))
+  print()
+
+  print("len dst http, mixed_http, single, range, list: ", len(dst_http_rules),  len(dst_mixed_http_rules),
+                                    len(dst_single_rules), len(dst_range_rules), len(dst_list_rules))
+  print("min dst http, mixed_http, single, range, list: ", min(dst_http_rules),  min(dst_mixed_http_rules),
+                                    min(dst_single_rules), min(dst_range_rules), min(dst_list_rules))
+  print("max dst http, mixed_http, single, range, list: ", max(dst_http_rules),  max(dst_mixed_http_rules),
+                                    max(dst_single_rules), max(dst_range_rules), max(dst_list_rules))
+
 
   with open("final_mapping.txt","w") as f:
     f.write(json.dumps(rules))
@@ -149,7 +207,7 @@ if __name__ == "__main__":
     f.write("\n")
 
     for r in rule_conds:
-      f.write("  if "+rule_conds[0]+"\n")
+      f.write("  if "+r+"\n")
       f.write("    return true;\n")
     f.write("  return false;\n")
     f.write("}\n")
