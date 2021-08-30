361196c44e86f0d8fb7b7a035344d976b6f405f1
Moein Khazraee <mkhazrae@cs.ucsd.edu>
2021-08-29 21:37:10 -0700

    Added pcap generator for pigasus test, reorganized the hls gen script.

--- fpga_src/accel/pigasus_sme/python/hls_gen.py
+++ fpga_src/accel/pigasus_sme/python/hls_gen.py
@@ -1,4 +1,6 @@
 import json
+import re
+from idstools.rule import parse
 
 HTTP_PORTS=[36,80,81,82,83,84,85,86,87,88,89,90,311,383,555,591,593,631,801,808,818,901,972,1158,1220,1414,1533,1741,1812,1830,1942,2231,2301,2381,2578,2809,2980,3029,3037,3057,3128,3443,3702,4000,4343,4848,5000,5117,5250,5450,5600,5814,6080,6173,6988,7000,7001,7005,7071,7144,7145,7510,7770,7777,7778,7779,8000,8001,8008,8014,8015,8020,8028,8040,8080,8081,8082,8085,8088,8090,8118,8123,8180,8181,8182,8222,8243,8280,8300,8333,8344,8400,8443,8500,8509,8787,8800,8888,8899,8983,9000,9002,9060,9080,9090,9091,9111,9290,9443,9447,9710,9788,9999,10000,11371,12601,13014,15489,19980,29991,33300,34412,34443,34444,40007,41080,44449,50000,50002,51423,53331,55252,55555,56712]
 
@@ -80,14 +82,20 @@ if __name__ == "__main__":
   rules = {}
   id_map = {}
   i = 1;
-  for line in open("rule_list","r"):
+  for line in open("selected_rules","r"):
     id_map[int(line)] = i
     i += 1
 
-  for line in open("extracted.txt","r"):
-    rule = line.split()
-    if int(rule[0]) in id_map:
-      rule_ID = id_map[int(rule[0])]
+  for line in open("all_rules.txt","r"):
+
+    sid = re.findall("sid:[0-9]+;", line)
+    sid = int(re.findall("[0-9]+", sid[0])[0])
+
+    header = parse(line)['header']
+    rule = header.split()
+
+    if sid in id_map:
+      rule_ID = id_map[sid]
       src_port = rule[3]
       dst_port = rule[6]
       if (rule[1]=="tcp"):
@@ -109,7 +117,7 @@ if __name__ == "__main__":
   with open("final_mapping.txt","w") as f:
     f.write(json.dumps(rules))
 
-  with open("../port_group.cc","w") as f:
+  with open("../HLS_test/port_group.cc","w") as f:
     f.write("#include \"ap_int.h\"\n")
     f.write("\n")
     f.write("typedef struct {\n")
---
+++ fpga_src/accel/pigasus_sme/python/pcap_gen.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+"""
+Copyright 2020, The Regents of the University of California.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS OF THE UNIVERSITY OF CALIFORNIA ''AS
+IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OF THE UNIVERSITY OF CALIFORNIA OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of The Regents of the University of California.
+"""
+
+import argparse
+import collections
+import os
+import re
+import random
+
+from idstools.rule import parse
+
+from scapy.layers.l2 import Ether
+from scapy.layers.inet import IP, UDP, TCP
+from scapy.utils import PcapWriter
+
+HTTP_PORTS=[36,80,81,82,83,84,85,86,87,88,89,90,311,383,555,591,593,631,801,808,818,901,972,1158,1220,1414,1533,1741,1812,1830,1942,2231,2301,2381,2578,2809,2980,3029,3037,3057,3128,3443,3702,4000,4343,4848,5000,5117,5250,5450,5600,5814,6080,6173,6988,7000,7001,7005,7071,7144,7145,7510,7770,7777,7778,7779,8000,8001,8008,8014,8015,8020,8028,8040,8080,8081,8082,8085,8088,8090,8118,8123,8180,8181,8182,8222,8243,8280,8300,8333,8344,8400,8443,8500,8509,8787,8800,8888,8899,8983,9000,9002,9060,9080,9090,9091,9111,9290,9443,9447,9710,9788,9999,10000,11371,12601,13014,15489,19980,29991,33300,34412,34443,34444,40007,41080,44449,50000,50002,51423,53331,55252,55555,56712]
+
+r = random.Random(521)
+
+def parse_content_string(s):
+    """
+    Parse content string and convert to byte string
+
+    Converts hex sections delineated with pipes
+
+    an example|20|string|0d 0a|
+
+    b'an example string\r\n'
+
+    """
+    b = bytearray()
+    offset = 0
+
+    while offset < len(s):
+        if '|' in s[offset:]:
+            rs = s.index('|', offset)
+            re = s.index('|', rs+1)
+
+            b.extend(s[offset:rs].encode('utf-8'))
+            b.extend(bytearray.fromhex(s[rs+1:re]))
+
+            offset = re+1
+        else:
+            b.extend(s[offset:].encode('utf-8'))
+            break
+
+    return bytes(b)
+
+
+def truncate_content(content, limit):
+  count = 0
+  byte_mode = 0
+  for i in range(len(content)):
+    if (content[i]=='|'):
+      if (byte_mode==0):
+        byte_mode = 1
+      else:
+        count += 1
+        byte_mode = 0
+    else:
+      if (byte_mode==0):
+        count += 1
+      else:
+        if (content[i]==' '):
+          count += 1
+
+    if (count==limit):
+      if (byte_mode==0):
+        return content[:i+1]
+      else:
+        return (content[:i]+'|')
+
+  return content
+
+
+def fast_pattern_extractor (line):
+  if (len(line.strip()) == 0):
+    return "", ""
+  else:
+    fast_pattern = ""
+    pattern_len  = 0
+    unknown_flag = False
+    fast_flag    = False
+
+    rule    = parse(line)
+    options = rule['options']
+    header  = rule['header']
+
+    sid = re.findall("sid:[0-9]+;", line)
+    sid = int(re.findall("[0-9]+", sid[0])[0])
+
+    for opt in options:
+      if opt['name'] == 'content':
+        value = re.findall('"([^"]*)"', opt['value'])[0]
+        flags = opt['value'][len(value)+3:]
+        cont_len = len(parse_content_string(value))
+
+        if "fast_pattern" in flags:
+          fast_pattern = value[:]
+          fast_flag = True
+          if opt['value'].startswith("!"):
+            unknown_flag = True
+          break
+
+        elif opt['value'].startswith("!"):
+
+          # Not acceptable for fast pattern
+          if (len(flags)!=0):
+            if ("relative" in flags) or ("case" in flags) or ("offset" in flags) or \
+              ("depth" in flags) or ("within" in flags) or ("distance" in flags):
+              continue
+
+          # Acceptable but not long enough
+          elif (cont_len <= pattern_len):
+              continue
+
+          # Acceptable, not sure what to do for hardware check
+          # might get overriden by a longer pattern
+          else:
+            unknown_flag = True
+            pattern_len = cont_len
+            continue
+
+        else:
+          # No \" in curent content rules, might not be always true
+          # re strips of the ! and ""
+          if (cont_len>pattern_len):
+            unknown_flag = False
+            fast_pattern = value[:]
+            pattern_len = cont_len
+
+      # Doesn't find it properly
+      # elif opt['name'] == 'sid':
+      #   sid = int(opt['value'])
+
+    # if unknown_flag:
+    #   print ("WARNING: used", fast_pattern, "for", line,'\n')
+
+    return fast_pattern, header, sid, unknown_flag
+
+def port_gen (query):
+  has_http = False
+  if ("$HTTP_PORTS," in query):
+    query = query.replace("$HTTP_PORTS,", "")
+    has_http = True
+  if (",$HTTP_PORTS" in query):
+    query = query.replace(",$HTTP_PORTS", "")
+    has_http = True
+  if (query.startswith("[")):
+    query = query[1:-1]
+
+  if (query=="$HTTP_PORTS"):
+    return r.choice(HTTP_PORTS)
+  elif (query=="$FILE_DATA_PORTS"):
+    return r.choice(HTTP_PORTS+[110, 143])
+  elif (query=="$FTP_PORTS"):
+    return r.choice([21, 2100, 3535])
+  elif (query=="$ORACLE_PORTS"):
+    return random.randint(1024, 65535)
+  elif (":" in query):
+    if (query[-1]==":"):
+      ports = [int(x) for x in query[:-1].split(",")]
+      if (len(ports)==1):
+        if (has_http):
+          return r.choice(list(range(ports[0],65535)) + HTTP_PORTS)
+        else:
+          return random.randint(ports[0], 65535)
+      else:
+        if (has_http):
+          return r.choice(ports[:-1]+HTTP_PORTS+list(range(ports[-1], 65535)))
+        else:
+          return r.choice(ports[:-1]+list(range(ports[-1], 65535)))
+    else:
+      ports = query.split(",")
+      if (len(ports)==1):
+        [low, high] = [int(x) for x in ports[0].split(":")]
+        if (has_http):
+          return r.choice(list(range(low, high)) + HTTP_PORTS)
+        else:
+          return random.randint(low, high)
+      elif (len(ports)==2):
+        if (has_http):
+          print("case not supported", query)
+          return -1
+        elif (":" in ports[0]):
+          [low, high] = [int(x) for x in ports[0].split(":")]
+          return r.choice(list(range(low, high)) + [int(ports[1])])
+        else:
+          [low, high] = [int(x) for x in ports[1].split(":")]
+          return r.choice(list(range(low, high)) + [int(ports[0])])
+      else:
+        print("case not supported")
+        return -1
+
+  elif (query=="any"):
+    return random.randint(0, 65535)
+  else:
+    ports = [int(x) for x in query.split(",")]
+    if (has_http):
+      return r.choice(HTTP_PORTS+ports)
+    else:
+      return r.choice(ports)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rules_file', type=str, default=[], action='append', help="Rules file")
+    parser.add_argument('--selected_file', type=str, default=None, help="Selected Rules file")
+    parser.add_argument('--headers_file', type=str, default=None, help="Headers file")
+    parser.add_argument('--output_pcap', type=str, default='attack.pcap', help="Pcap output file name")
+    parser.add_argument('--max_length', type=int, default=8, help="Max rule length")
+    args = parser.parse_args()
+
+    max_length = args.max_length
+    match_list = []
+
+    id_map = {}
+    i = 1;
+    if (args.selected_file):
+        for line in open(args.selected_file,"r"):
+            id_map[int(line)] = i
+            i += 1
+
+    rule_count = 0
+    dropped = 0
+
+    for fn in args.rules_file:
+        with open(fn, 'r') as f:
+            for w in f.read().splitlines():
+                pattern, header, sid, unknown = fast_pattern_extractor(w)
+                hdr = header.split()
+                prot     = hdr[1]
+
+                if (sid in id_map or not args.selected_file):
+                    if (unknown or not (prot=="tcp" or prot=="udp")):
+                        dropped += 1
+                    else:
+                        rule_count += 1
+                        b = parse_content_string(pattern)
+                        if not b:
+                            if w:
+                                print ("ERROR: No patterns found for", w,'\n')
+                            continue
+                        if max_length:
+                            b = b[:max_length]
+                            pattern=truncate_content(pattern,max_length)
+
+                        src_port = port_gen(hdr[3])
+                        dst_port = port_gen(hdr[6])
+                        match_list.append((b, prot, src_port, dst_port))
+
+    print ("Dropped rules: %d, Total filtered rules: %d" %(dropped, rule_count))
+
+    with PcapWriter(open(args.output_pcap, 'wb'), sync=True) as pcap:
+        eth = Ether(src='5A:51:52:53:54:55', dst='DA:D1:D2:D3:D4:D5')
+        ip = IP(src='192.168.1.100', dst='192.168.1.101')
+
+        # test packets that shouldn't match any rules
+        udp = UDP(sport=1234, dport=5678)
+        payload = bytes([x % 256 for x in range(256)])
+        pcap.write(eth / ip / udp / payload)
+
+        tcp = TCP(sport=1234, dport=5678)
+        payload = bytes([x % 256 for x in range(256)])
+        pcap.write(eth / ip / tcp / payload)
+
+        tcp = TCP(sport=12345, dport=80)
+        payload = bytes([x % 256 for x in range(256)])
+        pcap.write(eth / ip / tcp / payload)
+
+        tcp = TCP(sport=54321, dport=80)
+        payload = bytes([x % 256 for x in range(256)])
+        pcap.write(eth / ip / tcp / payload)
+
+        for rule in match_list:
+          payload, port, sport, dport = rule
+          if (port=='udp'):
+              udp = UDP(sport=sport, dport=dport)
+              payload += bytes([x % 256 for x in range(r.randint(64, 500)-len(payload))])
+              pcap.write(eth / ip / udp / payload)
+          else: #tcp
+              tcp = TCP(sport=sport, dport=dport)
+              payload += bytes([x % 256 for x in range(r.randint(64, 500)-len(payload))])
+              pcap.write(eth / ip / tcp / payload)
+
+if __name__ == "__main__":
+    main()
